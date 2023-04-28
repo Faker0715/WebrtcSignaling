559d6d57d072bac24b7a9e596f3be0e47a289c68
Faker0715 <949506563@qq.com>
2023-04-28 14:01:52 +0800

    [init] push client

---
+++ go.mod
@@ -0,0 +1,5 @@
+module signaling
+
+go 1.20
+
+
---
+++ src/router.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"signaling/src/action"
+	"signaling/src/framework"
+)
+
+func init() {
+	framework.GActionRouter["/signaling/push"] = action.NewPushAction()
+	framework.GActionRouter["/signaling/stoppush"] = action.NewStopPushAction()
+
+	framework.GActionRouter["/signaling/sendanswer"] = action.NewSendAnswerAction()
+	framework.GActionRouter["/xrtcclient/push"] = action.NewXrtcClientPushAction()
+}
